a50559a6f63263109878d8fc7e8789f01fe9bbfb
mohammadvaladbiegi2 <mohammadvaladbiegi2@gmail.com>
2024-12-04 22:44:07 +0330

    ser Module Config

---
+++ config/Module/module.config.ts
@@ -0,0 +1,8 @@
+import { ConfigModuleOptions } from "@nestjs/config";
+
+const moduleConfig = {
+    isGlobal: true
+} satisfies ConfigModuleOptions
+
+
+export { moduleConfig }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataBaseConfig } from 'config/DBconfig/postgresSQL.config';
+import { moduleConfig } from 'config/Module/module.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(dataBaseConfig)
+    TypeOrmModule.forRoot(dataBaseConfig),
+    ConfigModule.forRoot(moduleConfig)
   ]
 })
 export class AppModule { }
